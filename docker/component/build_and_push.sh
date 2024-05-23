3975f496f987cc380313c822e8de2ffbe1ca540f
Sebastian Scherer <scherersebastian@yahoo.de>
2024-05-23 16:21:47 +0100

    adding git based version tagging to docker imasge

--- docker/component/build_and_push.sh
+++ docker/component/build_and_push.sh
@@ -1,5 +1,6 @@
 export DOCKERTAG=bettmensch88/bettmensch.ai:3.11
+export BETTMENSCH_AI_VERSION="0d06b91"
 
-docker build -t ${DOCKERTAG} . 
+docker build -t ${DOCKERTAG}-${BETTMENSCH_AI_VERSION} --build-arg="BETTMENSCH_AI_VERSION=${BETTMENSCH_AI_VERSION}" . 
 
 docker push ${DOCKERTAG}
