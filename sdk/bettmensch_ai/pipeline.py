50b2887ed4f0b739e7fd6108cc43aa13b03a2d47
bettmensch88 <sebastian.scherer@yahoo.de>
2024-05-23 16:42:00 +0100

    argument output path now os independent

--- sdk/bettmensch_ai/pipeline.py
+++ sdk/bettmensch_ai/pipeline.py
@@ -466,7 +466,7 @@ def test_pipeline():
 
         first_sum = add(
             hera_template_kwargs={
-                "image": "bettmensch88/bettmensch.ai:3.11-2769afb"
+                "image": "bettmensch88/bettmensch.ai:3.11-5d2280a"
             },
             a=a,
             b=b,
@@ -474,7 +474,7 @@ def test_pipeline():
 
         second_sum = add(
             hera_template_kwargs={
-                "image": "bettmensch88/bettmensch.ai:3.11-2769afb"
+                "image": "bettmensch88/bettmensch.ai:3.11-5d2280a"
             },
             a=first_sum.outputs["sum"],
             b=c,
@@ -482,7 +482,7 @@ def test_pipeline():
 
         last_sum = add(
             hera_template_kwargs={
-                "image": "bettmensch88/bettmensch.ai:3.11-2769afb"
+                "image": "bettmensch88/bettmensch.ai:3.11-5d2280a"
             },
             a=second_sum.outputs["sum"],
             b=second_sum.outputs["sum"],
