4cb1b2247244374fc2ffc0493ee37e2230ae67f1
Sebastian Scherer <scherersebastian@yahoo.de>
2024-05-28 16:49:05 +0100

    added unit test output dir reference that works when running tests from top repo dir

--- sdk/test/unit/test_pipeline.py
+++ sdk/test/unit/test_pipeline.py
@@ -1,7 +1,7 @@
 from bettmensch_ai import InputParameter, OutputParameter, component, pipeline
 
 
-def test_parameter_pipeline():
+def test_parameter_pipeline(test_output_dir):
     @component
     def add(
         a: InputParameter, b: InputParameter, sum: OutputParameter = None
@@ -45,8 +45,6 @@ def test_parameter_pipeline():
             b=two_b.outputs["product"],
         )
 
-    print(f"Pipeline type: {type(a_plus_bc_plus_2b)}")
-
-    a_plus_bc_plus_2b.export("./outputs")
+    a_plus_bc_plus_2b.export(test_output_dir)
     # a_plus_bc_plus_2b.register()
     # a_plus_bc_plus_2b.run(a=3, b=2, c=1)
