0a75805972fe485ba9497dc14068567f3c3a029a
Ubuntu <ubuntu@ip-172-31-80-148.ec2.internal>
2024-05-28 17:15:31 +0000

    rebuilt image and updated build script to read in commit hash automatically

--- docker/component/build_and_push.sh
+++ docker/component/build_and_push.sh
@@ -1,5 +1,9 @@
-export DOCKERTAG=bettmensch88/bettmensch.ai:3.11
-export BETTMENSCH_AI_VERSION=${git rev-parse --short HEAD}
+DOCKERTAG=bettmensch88/bettmensch.ai:3.11
+BETTMENSCH_AI_VERSION=$(git rev-parse --short HEAD)
+
+echo "Docker base tag: ${DOCKERTAG}"
+echo "Commit hash: ${BETTMENSCH_AI_VERSION}"
+echo "Docker tag: ${DOCKERTAG}-${BETTMENSCH_AI_VERSION}"
 
 docker build -t ${DOCKERTAG}-latest -t ${DOCKERTAG}-${BETTMENSCH_AI_VERSION} --build-arg="BETTMENSCH_AI_VERSION=${BETTMENSCH_AI_VERSION}" . 
 
