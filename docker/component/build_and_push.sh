b1479ecdda1f436f8181be1e04a8f3f5db07cd64
Sebastian Scherer <scherersebastian@yahoo.de>
2024-05-28 17:10:47 +0000

    updated image

--- docker/component/build_and_push.sh
+++ docker/component/build_and_push.sh
@@ -1,5 +1,5 @@
 export DOCKERTAG=bettmensch88/bettmensch.ai:3.11
-export BETTMENSCH_AI_VERSION="master"
+export BETTMENSCH_AI_VERSION=${git rev-parse --short HEAD}
 
 docker build -t ${DOCKERTAG}-latest -t ${DOCKERTAG}-${BETTMENSCH_AI_VERSION} --build-arg="BETTMENSCH_AI_VERSION=${BETTMENSCH_AI_VERSION}" . 
 
