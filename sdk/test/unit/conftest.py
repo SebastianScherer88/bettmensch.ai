e0ed4713944ec014195de7c45e5887ab12706775
bettmensch88 <sebastian.scherer@yahoo.de>
2024-05-31 22:13:01 +0100

    refactored component unit tests

--- sdk/test/unit/conftest.py
+++ sdk/test/unit/conftest.py
@@ -1,6 +1,75 @@
 import pytest
+from bettmensch_ai import (
+    COMPONENT_TYPE,
+    PIPELINE_TYPE,
+    InputArtifact,
+    InputParameter,
+    OutputArtifact,
+    OutputParameter,
+)
 
 
 @pytest.fixture
-def test_output_dir():
-    return "sdk/test/outputs"
+def test_mock_pipeline():
+    class MockPipeline:
+        type = PIPELINE_TYPE
+        io_owner_name = PIPELINE_TYPE
+
+    return MockPipeline()
+
+
+@pytest.fixture
+def test_mock_component():
+    class MockComponent:
+        type = COMPONENT_TYPE
+        name = "mock-component-0"
+        io_owner_name = f"{type}.{name}"
+
+    return MockComponent()
+
+
+@pytest.fixture
+def test_convert_to_artifact_function():
+    def convert_to_artifact(
+        a: InputParameter,
+        b: InputParameter,
+        a_art: OutputArtifact = None,
+        b_art: OutputArtifact = None,
+    ) -> None:
+
+        with open(a_art.path, "w") as a_art_file:
+            a_art_file.write(str(a))
+
+        with open(b_art.path, "w") as b_art_file:
+            b_art_file.write(str(b))
+
+    return convert_to_artifact
+
+
+@pytest.fixture
+def test_show_artifact_function():
+    def show_artifact(a: InputArtifact, b: InputArtifact) -> None:
+
+        with open(a.path, "r") as a_art_file:
+            a_content = a_art_file.read()
+
+        with open(b.path, "r") as b_art_file:
+            b_content = b_art_file.read()
+
+        print(f"Content of input artifact a: {a_content}")
+        print(f"Content of input artifact b: {b_content}")
+
+    return show_artifact
+
+
+@pytest.fixture
+def test_add_function():
+    def add(
+        a: InputParameter = 1,
+        b: InputParameter = 2,
+        sum: OutputParameter = None,
+    ) -> None:
+
+        sum.assign(a + b)
+
+    return add
--- sdk/test/unit/test_component.py
+++ sdk/test/unit/test_component.py
@@ -1,5 +1,7 @@
 from bettmensch_ai import (
+    COMPONENT_TYPE,
     PIPELINE_TYPE,
+    Component,
     InputArtifact,
     InputParameter,
     OutputArtifact,
@@ -7,122 +9,193 @@ from bettmensch_ai import (
     _pipeline_context,
     component,
 )
-from hera.workflows import DAG, Parameter, Resources, WorkflowTemplate
+from hera.workflows import DAG, Parameter, WorkflowTemplate
 
 
-def test_artifact_component(test_output_dir):
-    """Declaration of Component using InputArtifact and OutputArtifact"""
+def test_component___init__(test_mock_pipeline, test_mock_component):
+    """Tests of Component constructor."""
 
-    @component
-    def convert_to_artifact(
-        a_param: InputParameter,
-        b_param: InputParameter,
-        a_art: OutputArtifact = None,
-        b_art: OutputArtifact = None,
-    ) -> None:
-
-        with open(a_art.path, "w") as a_art_file:
-            a_art_file.write(str(a_param))
-
-        with open(b_art.path, "w") as b_art_file:
-            b_art_file.write(str(b_param))
-
-    @component
-    def show_artifact(a: InputArtifact, b: InputArtifact) -> None:
-
-        with open(a.path, "r") as a_art_file:
-            a_content = a_art_file.read()
-
-        with open(b.path, "r") as b_art_file:
-            b_content = b_art_file.read()
-
-        print(f"Content of input artifact a: {a_content}")
-        print(f"Content of input artifact b: {b_content}")
-
-    class MockPipeline:
-        type: str = PIPELINE_TYPE
-        io_owner_name: str = PIPELINE_TYPE
-
-    # mock active pipeline with 3 inputs
-    pipeline_input_a = InputParameter(name="a", value=1)
-    pipeline_input_a.set_owner(MockPipeline())
-    pipeline_input_b = InputParameter(name="b", value=2)
-    pipeline_input_b.set_owner(MockPipeline())
+    def test_function(
+        a: InputParameter,
+        b: InputParameter,
+        c: InputParameter,
+        d: InputArtifact,
+        a_out: OutputParameter,
+        b_out: OutputArtifact,
+    ):
+        pass
 
     _pipeline_context.activate()
     _pipeline_context.clear()
 
-    # add components to pipeline context
-    convert = convert_to_artifact(
-        "convert_to_artifact",
-        a_param=pipeline_input_a,
-        b_param=pipeline_input_b,
-    )
+    test_input_a = InputParameter("fixed", 1)
+    test_input_b = InputParameter("mock_pipe_in", 1)
+    test_input_b.set_owner(test_mock_pipeline)
+    test_input_c = OutputParameter("mock_comp_out_param")
+    test_input_c.set_owner(test_mock_component)
+    test_input_d = OutputArtifact("mock_comp_out_art")
+    test_input_d.set_owner(test_mock_component)
 
-    show = show_artifact(
-        "show_artifact",
-        a=convert.outputs["a_art"],
-        b=convert.outputs["b_art"],
+    task_inputs = {
+        "a": test_input_a,
+        "b": test_input_b,
+        "c": test_input_c,
+        "d": test_input_d,
+    }
+
+    # add components to pipeline context
+    test_component = Component(
+        func=test_function, name="test_name", **task_inputs
     )
 
     # close pipeline context
     _pipeline_context.deactivate()
 
-    with WorkflowTemplate(
-        name="test-artifact-component-workflow-template",
-        entrypoint="test_dag",
-        namespace="argo",
-        arguments=[
-            Parameter(name="a", value="Parameter A"),
-            Parameter(name="b", value="Parameter B"),
-        ],
-    ) as wft:
+    # validate addition of component to pipeline context
+    assert test_component == _pipeline_context.components[0]
 
-        with DAG(name="test_dag"):
-            convert.to_hera()
-            show.to_hera()
+    # validate component attributes
+    assert isinstance(test_component, Component)
+    assert test_component.base_name == "test-name"
+    assert test_component.name == "test-name-0"
+    assert test_component.func == test_function
+    assert test_component.hera_template_kwargs == {}
+    assert test_component.depends == "mock-component-0"
 
-    wft.to_file(test_output_dir)
+    # validate component task_inputs
+    for task_input_name in ("a", "b", "c", "d"):
+        assert (
+            test_component.task_inputs[task_input_name].name == task_input_name
+        )
+        assert (
+            test_component.task_inputs[task_input_name].owner == test_component
+        )
+        assert (
+            test_component.task_inputs[task_input_name].source
+            is task_inputs[task_input_name]
+        )
+
+    assert test_component.task_inputs["a"].value == task_inputs["a"].value
+    assert test_component.task_inputs["b"].value == task_inputs["b"].value
+    assert test_component.task_inputs["c"].value is None
+
+    # validate component template_inputs
+    assert list(test_component.template_inputs.keys()) == ["d"]
+    isinstance(test_component.template_inputs["d"], InputArtifact)
+    test_component.template_inputs["d"].name = "d"
+
+    # validate component template_outputs
+    assert test_component.template_outputs["a_out"].owner == test_component
+    assert isinstance(test_component.template_outputs["a_out"], OutputParameter)
+    assert test_component.template_outputs["b_out"].owner == test_component
+    assert isinstance(test_component.template_outputs["b_out"], OutputArtifact)
+
+    assert test_component.task_factory is not None
 
 
-def test_parameter_component(test_output_dir):
+def test_component_decorator(test_mock_pipeline, test_mock_component):
+    """Tests of Component constructor."""
+
+    def test_function(
+        a: InputParameter,
+        b: InputParameter,
+        c: InputParameter,
+        d: InputArtifact,
+        a_out: OutputParameter,
+        b_out: OutputArtifact,
+    ):
+        pass
+
+    test_component_factory = component(test_function)
+
+    _pipeline_context.activate()
+    _pipeline_context.clear()
+
+    test_input_a = InputParameter("fixed", 1)
+    test_input_b = InputParameter("mock_pipe_in", 1)
+    test_input_b.set_owner(test_mock_pipeline)
+    test_input_c = OutputParameter("mock_comp_out_param")
+    test_input_c.set_owner(test_mock_component)
+    test_input_d = OutputArtifact("mock_comp_out_art")
+    test_input_d.set_owner(test_mock_component)
+
+    task_inputs = {
+        "a": test_input_a,
+        "b": test_input_b,
+        "c": test_input_c,
+        "d": test_input_d,
+    }
+
+    # add components to pipeline context
+    test_component = test_component_factory(name="test_name", **task_inputs)
+
+    # close pipeline context
+    _pipeline_context.deactivate()
+
+    # validate addition of component to pipeline context
+    assert test_component == _pipeline_context.components[0]
+
+    # validate component attributes
+    assert isinstance(test_component, Component)
+    assert test_component.base_name == "test-name"
+    assert test_component.name == "test-name-0"
+    assert test_component.func == test_function
+    assert test_component.hera_template_kwargs == {}
+
+    # validate component task_inputs
+    for task_input_name in ("a", "b", "c", "d"):
+        assert (
+            test_component.task_inputs[task_input_name].name == task_input_name
+        )
+        assert (
+            test_component.task_inputs[task_input_name].owner == test_component
+        )
+        assert (
+            test_component.task_inputs[task_input_name].source
+            is task_inputs[task_input_name]
+        )
+
+    assert test_component.task_inputs["a"].value == task_inputs["a"].value
+    assert test_component.task_inputs["b"].value == task_inputs["b"].value
+    assert test_component.task_inputs["c"].value is None
+
+    # validate component template_inputs
+    assert list(test_component.template_inputs.keys()) == ["d"]
+    isinstance(test_component.template_inputs["d"], InputArtifact)
+    test_component.template_inputs["d"].name = "d"
+
+    # validate component template_outputs
+    assert test_component.template_outputs["a_out"].owner == test_component
+    assert isinstance(test_component.template_outputs["a_out"], OutputParameter)
+    assert test_component.template_outputs["b_out"].owner == test_component
+    assert isinstance(test_component.template_outputs["b_out"], OutputArtifact)
+
+    assert test_component.task_factory is not None
+
+
+def test_component_to_hera(test_add_function, test_mock_pipeline):
     """Declaration of Component using InputParameter and OutputParameter"""
 
-    @component
-    def add(
-        a: InputParameter = 1,
-        b: InputParameter = 2,
-        sum: OutputParameter = None,
-    ) -> None:
-
-        sum.assign(a + b)
-
-    class MockPipeline:
-        type: str = PIPELINE_TYPE
-        io_owner_name: str = PIPELINE_TYPE
+    add_component_factory = component(test_add_function)
 
     # mock active pipeline with 3 inputs
     pipeline_input_a = InputParameter(name="a", value=1)
-    pipeline_input_a.set_owner(MockPipeline())
+    pipeline_input_a.set_owner(test_mock_pipeline)
     pipeline_input_b = InputParameter(name="b", value=2)
-    pipeline_input_b.set_owner(MockPipeline())
+    pipeline_input_b.set_owner(test_mock_pipeline)
 
     _pipeline_context.activate()
     _pipeline_context.clear()
 
     # add components to pipeline context
-    a_plus_b = add(
+    a_plus_b = add_component_factory(
         "a_plus_b",
         a=pipeline_input_a,
         b=pipeline_input_b,
-        hera_template_kwargs={"resources": Resources(cpu_request=0.2)},
     )
 
-    a_plus_b_plus_2 = add(
-        "a_plus_b_plus_2",
-        a=a_plus_b.outputs["sum"],
-        b=InputParameter("two", 2),
-        hera_template_kwargs={"resources": Resources(cpu_request=0.3)},
+    a_plus_b_plus_2 = add_component_factory(
+        "a_plus_b_plus_2", a=a_plus_b.outputs["sum"], b=InputParameter("two", 2)
     )
 
     # close pipeline context
@@ -142,4 +215,8 @@ def test_parameter_component(test_output_dir):
             a_plus_b.to_hera()
             a_plus_b_plus_2.to_hera()
 
-    wft.to_file(test_output_dir)
+    task_names = [task.name for task in wft.templates[0].tasks]
+    assert task_names == ["a-plus-b-0", "a-plus-b-plus-2-0"]
+
+    script_template_names = [template.name for template in wft.templates[1:]]
+    assert script_template_names == ["a-plus-b", "a-plus-b-plus-2"]
