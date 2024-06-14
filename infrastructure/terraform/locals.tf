3bb08f3c70c9ef99b632e586c9964bcf804983df
Ubuntu <ubuntu@ip-172-31-80-148.ec2.internal>
2024-06-14 22:59:07 +0000

    moved eks cluster into us-east-2 to get GPU resources. separately asked for quota increases. also fixed the karpenter manifests

--- infrastructure/terraform/locals.tf
+++ infrastructure/terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   name            = "bettmensch-ai"
   cluster_version = "1.29"
-  region          = "us-east-1"
+  region          = "us-east-2"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
