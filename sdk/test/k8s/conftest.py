d393557184003c7aeee29a68b9f8926e6fda679d
Sebastian Scherer <scherersebastian@yahoo.de>
2024-07-19 17:28:39 +0000

    moving some tests around and adding lightning test suite

--- sdk/test/k8s/conftest.py
+++ sdk/test/k8s/conftest.py
@@ -6,3 +6,8 @@ import pytest
 @pytest.fixture
 def test_output_dir():
     return os.path.join(".", "sdk", "test", "integration", "outputs")
+
+
+@pytest.fixture
+def test_namespace():
+    return "argo"
