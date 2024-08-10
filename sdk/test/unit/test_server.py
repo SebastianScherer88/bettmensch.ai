0e80f560a3111e0446cb4c1bd039fd2d4cc2308d
Sebastian Scherer <scherersebastian@yahoo.de>
2024-08-10 23:57:15 +0000

    added unit tests for server Flow and Pipeline parsing methods

--- sdk/test/unit/test_server.py
+++ sdk/test/unit/test_server.py
@@ -2,15 +2,49 @@ from bettmensch_ai.server.flow import Flow
 from bettmensch_ai.server.pipeline import Pipeline as RegisteredPipeline
 
 
-def test_pipeline_from_hera_workflow_template_model(
-    test_hera_workflow_template_model,
+def test_pipeline_from_hera_artifact_workflow_template_model(
+    test_hera_artifact_workflow_template_model,
 ):
 
     RegisteredPipeline.from_hera_workflow_template_model(
-        test_hera_workflow_template_model
+        test_hera_artifact_workflow_template_model
     )
 
 
-def test_pipeline_from_hera_workflow_model(test_hera_workflow_model):
+def test_pipeline_from_hera_parameter_workflow_template_model(
+    test_hera_parameter_workflow_template_model,
+):
 
-    Flow.from_hera_workflow_model(test_hera_workflow_model)
+    RegisteredPipeline.from_hera_workflow_template_model(
+        test_hera_parameter_workflow_template_model
+    )
+
+
+def test_pipeline_from_hera_torch_gpu_workflow_template_model(
+    test_hera_torch_gpu_workflow_template_model,
+):
+
+    RegisteredPipeline.from_hera_workflow_template_model(
+        test_hera_torch_gpu_workflow_template_model
+    )
+
+
+def test_pipeline_from_hera_artifact_workflow_model(
+    test_hera_artifact_workflow_model,
+):
+
+    Flow.from_hera_workflow_model(test_hera_artifact_workflow_model)
+
+
+def test_pipeline_from_hera_parameter_workflow_model(
+    test_hera_parameter_workflow_model,
+):
+
+    Flow.from_hera_workflow_model(test_hera_parameter_workflow_model)
+
+
+def test_pipeline_from_hera_torch_gpu_workflow_model(
+    test_hera_torch_gpu_workflow_model,
+):
+
+    Flow.from_hera_workflow_model(test_hera_torch_gpu_workflow_model)
