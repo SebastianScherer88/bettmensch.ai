be774be024b1d98fe74daa897f4f1f7d4194ea03
Sebastian Scherer <scherersebastian@yahoo.de>
2024-08-10 23:59:38 +0000

    using server module names for flow and pipeline in unit tests

--- sdk/test/unit/test_server.py
+++ sdk/test/unit/test_server.py
@@ -1,5 +1,4 @@
-from bettmensch_ai.server.flow import Flow
-from bettmensch_ai.server.pipeline import Pipeline as RegisteredPipeline
+from bettmensch_ai.server import RegisteredFlow, RegisteredPipeline
 
 
 def test_pipeline_from_hera_artifact_workflow_template_model(
@@ -33,18 +32,18 @@ def test_pipeline_from_hera_artifact_workflow_model(
     test_hera_artifact_workflow_model,
 ):
 
-    Flow.from_hera_workflow_model(test_hera_artifact_workflow_model)
+    RegisteredFlow.from_hera_workflow_model(test_hera_artifact_workflow_model)
 
 
 def test_pipeline_from_hera_parameter_workflow_model(
     test_hera_parameter_workflow_model,
 ):
 
-    Flow.from_hera_workflow_model(test_hera_parameter_workflow_model)
+    RegisteredFlow.from_hera_workflow_model(test_hera_parameter_workflow_model)
 
 
 def test_pipeline_from_hera_torch_gpu_workflow_model(
     test_hera_torch_gpu_workflow_model,
 ):
 
-    Flow.from_hera_workflow_model(test_hera_torch_gpu_workflow_model)
+    RegisteredFlow.from_hera_workflow_model(test_hera_torch_gpu_workflow_model)
